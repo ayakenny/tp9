4b24038aac471d50e39891c25d3b113126ed841e
AYAM Kenny <kenny.ayam@outlook.fr>
2023-03-29 22:44:47 +1100

    Réalisation du TP9 et apports de mofications sur le CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputDecoratorComponentComponent } from './input-decorator-component/input-decorator-component.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { InputDecoratorComponentComponent } from './input-decorator-component/in
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/input-decorator-component/input-decorator-component.component.ts
+++ src/app/input-decorator-component/input-decorator-component.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, ElementRef} from '@angular/core';
+import { faAddressCard, faShield, faEnvelope, faFile, faPen} from "@fortawesome/free-solid-svg-icons"
 
 @Component({
   selector: 'app-input-decorator-component',
   templateUrl: './input-decorator-component.component.html',
   styleUrls: ['./input-decorator-component.component.scss']
 })
-export class InputDecoratorComponentComponent {
 
+export class InputDecoratorComponentComponent implements AfterContentInit {
+  @ContentChild('input')
+  input!: ElementRef;
+  
+  currentIcon:any;
+  
+  constructor(){}
+  
+  ngAfterContentInit(): void {
+    console.log(this.input.nativeElement.type);
+    
+    if(this.input.nativeElement.type == 'text')
+      this.currentIcon = faAddressCard;
+    else if(this.input.nativeElement.type == 'password')
+      this.currentIcon = faShield;
+    else if(this.input.nativeElement.type == 'email')
+      this.currentIcon = faEnvelope;
+    else if(this.input.nativeElement.type == 'file')
+      this.currentIcon = faFile;
+    else if(this.input.nativeElement.type == "textarea")
+      this.currentIcon = faPen;
+    
+  }
+  
+  getIcon(){
+    return this.currentIcon;
+  }
 }
